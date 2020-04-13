226a705d7abfd4f35d16ffacde140fa3c5d650c6
Kimhwangho <revelcom@naver.com>
2020-04-13 17:02:41 +0900

    합합합

--- src/project/Test.java
+++ src/project/Test.java
@@ -1,5 +1,22 @@
 package project;
 
-public class Test {
 
+
+public class Test {
+	
+	public static void main(String[] args) {
+		
+		int sum=0;
+		for(int i=1;i<11;i++) {
+			sum+=i;
+		}
+		System.out.println("1~10까지의 합 : "+sum);
+		System.out.println();
+		sum=0;
+		for(int i=1;i<101;i++) {
+			sum+=i;
+		}
+		System.out.println("1~100까지의 합 : "+sum);
+		
+ }
 }
