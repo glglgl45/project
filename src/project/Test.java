f1167c16960d1f4e139f9065d81372be9ef1acfd
yoonjunghwan <glglgl47@naver.com>
2020-04-13 17:15:45 +0900

    수정2

--- src/project/Test.java
+++ src/project/Test.java
@@ -4,8 +4,9 @@ public class Test {
 	private int x;
 
 	public static void main(String[] args) {
-		
+
 		System.out.println("수정04131714");
+		System.out.println("수정2");
 		int sum=0;
 		for(int i=1;i<11;i++) {
 			sum+=i;
