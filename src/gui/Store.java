7a0e3bbca165b9ded30ed1148316965e94a08c4e
Jjae923 <hee94923@naver.com>
2020-04-22 15:27:46 +0900

    commit

--- src/gui/Store.java
+++ src/gui/Store.java
@@ -56,7 +56,7 @@ public class Store extends JFrame {
 	 */
 	public Store() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 700, 600);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
