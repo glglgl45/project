6c127a76fb35bd34b49c6c7624aa29ad8afaee7a
Kimhwangho <revelcom@naver.com>
2020-04-24 17:04:50 +0900

    login 화면 수정

--- src/gui/Login.java
+++ src/gui/Login.java
@@ -1,139 +1,175 @@
 package gui;
 
 import java.awt.BorderLayout;
+import java.awt.Dimension;
 import java.awt.EventQueue;
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.net.URL;
 
+import javax.imageio.ImageIO;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 import javax.swing.border.EmptyBorder;
 
 import dbguide.ClickerDAO;
 import dbguide.ClickerUserVO;
-import dbguide.UserVO;
 import system.Pickax;
-
-import java.awt.GridLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
-import javax.swing.JLabel;
-import javax.swing.JOptionPane;
-import javax.swing.JTextField;
-import javax.swing.JButton;
-import javax.swing.SwingConstants;
-import java.awt.Component;
-import javax.swing.Box;
-import java.awt.FlowLayout;
+import java.awt.Font;
 import javax.swing.JPasswordField;
 
-public class Login extends JFrame implements ActionListener {
 
-	private JPanel mPanel;
-	private JTextField txtID;
-	private JPasswordField txtPW;
-	private JButton btnLog, btnBack;
 	
-	private ClickerDAO dao;
-	private Pickax pick;
-	private ClickerUserVO vo;
-
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					Login frame = new Login();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
+	class BackImg1 extends JPanel {
+		
+		private JPanel ContentPane;
+		
+		private ClickerDAO dao;
+		private Pickax pick;
+		private ClickerUserVO vo;
+		
+		private BufferedImage img;
+		
+		public BackImg1() {
+			setLayout(null);
+			try {
+				URL url = getClass().getResource("main-demo.png");
+				img = ImageIO.read(new File(url.getFile()));
+			} catch (Exception e) {
+				e.printStackTrace();
 			}
-		});
+		}
+		
+		@Override
+		protected void paintComponent(Graphics g) {
+			super.paintComponent(g);
+			g.drawImage(img, 0, 0, null);
+		}
+		
+		@Override
+		public Dimension getPreferredSize() {
+			if(img==null) {
+				return new Dimension(100, 100);
+			}
+			return new Dimension(img.getWidth(), img.getHeight());
+		}
 	}
+	
+	
+	
+	
+	
+	public class Login extends JFrame implements ActionListener {
+
+		private JPanel contentPane;
+		private JButton btnLogin;
+		private JTextField textField;
+		private JTextField textField_1;
+		private JTextField txtId;
+		private JPasswordField txtPw;
+
+		public static void main(String[] args) {
+			EventQueue.invokeLater(new Runnable() {
+				public void run() {
+					try {
+						MainPage frame = new MainPage();
+						frame.setVisible(true);
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+				}
+			});
+		}
 
 	/**
 	 * Create the frame.
 	 */
 	public Login() {
-		pick = new Pickax();
-		dao=new ClickerDAO();
+		Pickax pick = new Pickax();
+		ClickerDAO dao = new ClickerDAO();
 		
-//		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 700, 600);
-		mPanel = new JPanel();
-		mPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
-		mPanel.setLayout(new BorderLayout(0, 0));
-		setContentPane(mPanel);
-		setVisible(true);
-		dao=new ClickerDAO();
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0,0));
+		setContentPane(contentPane);
 		
-		JPanel panel = new JPanel();
-		FlowLayout flowLayout = (FlowLayout) panel.getLayout();
-		flowLayout.setAlignment(FlowLayout.LEFT);
-		mPanel.add(panel, BorderLayout.NORTH);
 		
-		JLabel lblNewLabel_2 = new JLabel("로그인이 필요합니다");
-		panel.add(lblNewLabel_2);
+		JPanel backPanel = new BackImg1();
+		contentPane.add(backPanel,BorderLayout.CENTER);
 		
-		JPanel panel_1 = new JPanel();
-		mPanel.add(panel_1, BorderLayout.CENTER);
-		panel_1.setLayout(new GridLayout(0, 2, 0, 0));
+		JLabel lblNewLabel = new JLabel("아이디");
+		lblNewLabel.setFont(new Font("굴림", Font.BOLD, 30));
+		lblNewLabel.setBounds(304, 255, 97, 35);
+		backPanel.add(lblNewLabel);
 		
-		JLabel lblNewLabel = new JLabel("ID");
-		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
-		panel_1.add(lblNewLabel);
+		JButton btnNewButton = new JButton("로그인");
+		btnNewButton.setFont(new Font("굴림", Font.BOLD, 12));
+		btnNewButton.setBounds(246, 477, 97, 23);
+		backPanel.add(btnNewButton);
 		
-		txtID = new JTextField();
-		panel_1.add(txtID);
-		txtID.setColumns(10);
+		JButton btnNewButton_1 = new JButton("돌아가기");
+		btnNewButton_1.setFont(new Font("굴림", Font.BOLD, 12));
+		btnNewButton_1.setBounds(380, 477, 97, 23);
+		backPanel.add(btnNewButton_1);
 		
-		JLabel lblNewLabel_1 = new JLabel("Password");
-		lblNewLabel_1.setHorizontalAlignment(SwingConstants.CENTER);
-		panel_1.add(lblNewLabel_1);
-		
-		txtPW = new JPasswordField();
-		panel_1.add(txtPW);
-		
-		JPanel panel_2 = new JPanel();
-		mPanel.add(panel_2, BorderLayout.SOUTH);
-		
-		btnLog = new JButton("로그인");
-		panel_2.add(btnLog);
-		
-		btnBack = new JButton("돌아가기");
-		btnBack.addActionListener(new ActionListener() {
+		btnNewButton_1.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				dispose();
-				MainPage mp = new MainPage();
+				MainPage m = new MainPage();
 			}
 		});
-		panel_2.add(btnBack);
 		
-		btnLog.addActionListener(this);
-		txtPW.addActionListener(this);
+		txtId = new JTextField();
+		txtId.setBounds(296, 300, 116, 21);
+		backPanel.add(txtId);
+		txtId.setColumns(10);
+		
+		JLabel lblNewLabel_1 = new JLabel("비밀번호");
+		lblNewLabel_1.setFont(new Font("굴림", Font.BOLD, 30));
+		lblNewLabel_1.setBounds(296, 351, 128, 23);
+		backPanel.add(lblNewLabel_1);
+		
+		txtPw = new JPasswordField();
+		txtPw.setBounds(304, 389, 105, 21);
+		backPanel.add(txtPw);
+		setVisible(true);
+		dao=new ClickerDAO();
+		
 	}
 
+	
+	
 	@Override
-	public void actionPerformed(ActionEvent e) {
+	public void actionPerformed(ActionEvent e) {		
 		
-		vo=dao.searchUser(txtID.getText());	//DB에 없는 아이디 입력 시 에러 발생  처리방법 필요
-		if(e.getActionCommand().equals("로그인") || e.getSource()==txtPW) {
-			if(vo.getId().equals(txtID.getText()) && vo.getPwd().equals(new String(txtPW.getPassword()))) {		
-				pick.setUserId(txtID.getText());
-				vo=dao.searchUser(txtID.getText());
-				pick.setMoney(vo.getGold());	
-				pick.setDmg(vo.getDamage());
-				pick.setDura(vo.getDurability());
-				pick.setLevel(vo.getEnhance());
-				pick.setMul(vo.getMul());
-				pick.setPickName(vo.getPickName());
-				pick.setScore(vo.getScore());
-				pick.setPickLevel(vo.getPickLevel());
+		ClickerDAO dao = new ClickerDAO();
+		ClickerUserVO vo = dao.searchUser(txtId.getText());	//DB에 없는 아이디 입력 시 에러 발생  처리방법 필요
+
+		if(e.getActionCommand().equals("로그인") || e.getSource()==txtPw) {
+			if(((ClickerUserVO) vo).getId().equals(txtId.getText()) && ((ClickerUserVO) vo).getPwd().equals(new String(txtPw.getPassword()))) {		
+				Pickax pick = new Pickax();
+				pick.setUserId(txtId.getText());
+				vo=dao.searchUser(txtId.getText());
+				pick.setMoney(((ClickerUserVO) vo).getGold());	
+				pick.setDmg(((ClickerUserVO) vo).getDamage());
+				pick.setDura(((ClickerUserVO) vo).getDurability());
+				pick.setLevel(((ClickerUserVO) vo).getEnhance());
+				pick.setMul(((ClickerUserVO) vo).getMul());
+				pick.setPickName(((ClickerUserVO) vo).getPickName());
+				pick.setScore(((ClickerUserVO) vo).getScore());
+				pick.setPickLevel(((ClickerUserVO) vo).getPickLevel());
 				dispose();
-				MiddlePage mp = new MiddlePage();
+			//	MiddlePage mp = new MiddlePage();
 			}else {
 				JOptionPane.showMessageDialog(this, "로그인 정보를 확인해 주세요.");
 			}
--- src/gui/SignUp.java
+++ src/gui/SignUp.java
@@ -1,166 +1,217 @@
 package gui;
 
 import java.awt.BorderLayout;
+import java.awt.Dimension;
 import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.net.URL;
 
+import javax.imageio.ImageIO;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
 import javax.swing.border.EmptyBorder;
 
 import dbguide.ClickerDAO;
 import dbguide.ClickerUserVO;
 import system.Pickax;
 
-import java.awt.GridLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
-import javax.swing.JLabel;
-import javax.swing.JOptionPane;
-import javax.swing.JTextField;
-import javax.swing.JButton;
-import javax.swing.SwingConstants;
-import java.awt.Component;
-import javax.swing.Box;
-import java.awt.FlowLayout;
-import javax.swing.JPasswordField;
-
-public class SignUp extends JFrame implements ActionListener {
-
-	private JPanel contentPane;
-	private JTextField txtID;
-	private JPasswordField txtPW;
-	private JPasswordField passwordField;
-	private JPasswordField passwordField_1;
-	private JButton btnSignUp, btnBack;
-	private JTextField textField;
 	
-	private ClickerDAO dao;
-	private Pickax pick;
-	private ClickerUserVO vo;
-
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					SignUp frame = new SignUp();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
+	class BackImg2 extends JPanel {
+		
+		private JPanel ContentPane;
+		
+		private Pickax pick;
+	
+		private JButton btnBack;
+		
+		private BufferedImage img;
+		
+		private ClickerUserVO vo;
+		
+		public BackImg2() {
+			setLayout(null);
+			try {
+				URL url = getClass().getResource("main-demo.png");
+				img = ImageIO.read(new File(url.getFile()));
+			} catch (Exception e) {
+				e.printStackTrace();
 			}
-		});
+		}
+		
+		@Override
+		protected void paintComponent(Graphics g) {
+			super.paintComponent(g);
+			g.drawImage(img, 0, 0, null);
+		}
+		
+		@Override
+		public Dimension getPreferredSize() {
+			if(img==null) {
+				return new Dimension(100, 100);
+			}
+			return new Dimension(img.getWidth(), img.getHeight());
+		}
 	}
+	
+	
+	
+	
+	
+	public class SignUp extends JFrame implements ActionListener{
+
+		private JPanel contentPane;
+		private JButton btnLogin;
+		private JTextField textField;
+		private JTextField textField_1;
+		private JTextField txtID;
+		private JTextField passwordField;
+		private JTextField passwordField1;
+		private Pickax pick;
+		private ClickerDAO dao;
+		private ClickerUserVO vo;
+
+		public static void main(String[] args) {
+			EventQueue.invokeLater(new Runnable() {
+				public void run() {
+					try {
+						MainPage frame = new MainPage();
+						frame.setVisible(true);
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+				}
+			});
+		}
 
 	/**
 	 * Create the frame.
 	 */
 	public SignUp() {
-//		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); -- x키 누르면 같이 꺼짐
+		Pickax pick = new Pickax();
+		ClickerDAO dao = new ClickerDAO();
+		
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 700, 600);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
-		setVisible(true);
+		contentPane.setLayout(new BorderLayout(0,0));
 		setContentPane(contentPane);
 		
-		dao=new ClickerDAO();
-		pick=new Pickax();
-		JPanel panel = new JPanel();
-		FlowLayout flowLayout = (FlowLayout) panel.getLayout();
-		flowLayout.setAlignment(FlowLayout.LEFT);
-		contentPane.add(panel, BorderLayout.NORTH);
 		
-		JLabel lblNewLabel_2 = new JLabel("회원가입을 해주세요");
-		panel.add(lblNewLabel_2);
-		
-		JPanel panel_1 = new JPanel();
-		contentPane.add(panel_1, BorderLayout.CENTER);
-		panel_1.setLayout(new GridLayout(0, 2, 0, 0));
+		JPanel backPanel = new BackImg1();
+		contentPane.add(backPanel,BorderLayout.CENTER);
 		
 		JLabel lblNewLabel = new JLabel("아이디");
 		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
-		panel_1.add(lblNewLabel);
+		lblNewLabel.setFont(new Font("굴림", Font.BOLD, 30));
+		lblNewLabel.setBounds(304, 244, 97, 35);
+		backPanel.add(lblNewLabel);
+		
+		JButton btnSignUp = new JButton("회원가입");
+		btnSignUp.setFont(new Font("굴림", Font.BOLD, 12));
+		btnSignUp.setBounds(246, 477, 97, 23);
+		backPanel.add(btnSignUp);
+		
 		
 		txtID = new JTextField();
-		panel_1.add(txtID);
+		txtID.setBounds(296, 284, 116, 21);
+		backPanel.add(txtID);
 		txtID.setColumns(10);
+		
+		passwordField = new JTextField();
+		passwordField.setBounds(296, 351, 116, 21);
+		backPanel.add(passwordField);
+		passwordField.setColumns(10);
 		
 		JLabel lblNewLabel_1 = new JLabel("비밀번호");
 		lblNewLabel_1.setHorizontalAlignment(SwingConstants.CENTER);
-		panel_1.add(lblNewLabel_1);
+		lblNewLabel_1.setFont(new Font("굴림", Font.BOLD, 30));
+		lblNewLabel_1.setBounds(293, 317, 128, 23);
+		backPanel.add(lblNewLabel_1);
 		
-		passwordField = new JPasswordField();
-		panel_1.add(passwordField);
+		JLabel lblNewLabel_2 = new JLabel("비밀번호확인");
+		lblNewLabel_2.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel_2.setFont(new Font("굴림", Font.BOLD, 30));
+		lblNewLabel_2.setBounds(255, 385, 204, 35);
+		backPanel.add(lblNewLabel_2);
 		
-		JLabel lblNewLabel_3 = new JLabel("비밀번호 확인");
-		lblNewLabel_3.setHorizontalAlignment(SwingConstants.CENTER);
-		panel_1.add(lblNewLabel_3);
+		passwordField1 = new JTextField();
+		passwordField1.setBounds(296, 430, 116, 21);
+		backPanel.add(passwordField1);
+		passwordField1.setColumns(10);
+		setVisible(true);
+		dao=new ClickerDAO();
 		
-		passwordField_1 = new JPasswordField();
-		panel_1.add(passwordField_1);
-		
-		JPanel panel_2 = new JPanel();
-		contentPane.add(panel_2, BorderLayout.SOUTH);
-		
-		btnSignUp = new JButton("회원가입");
-		panel_2.add(btnSignUp);
-		
-		btnBack = new JButton("돌아가기");
+		JButton btnBack = new JButton("돌아가기");
+		btnBack.setFont(new Font("굴림", Font.BOLD, 12));
+		btnBack.setBounds(380, 477, 97, 23);
+		backPanel.add(btnBack);
 		btnBack.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				dispose();
-				MainPage mp = new MainPage();
+				MainPage m = new MainPage();
 			}
 		});
-		panel_2.add(btnBack);
-		
-		btnSignUp.addActionListener(this);
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		if(e.getActionCommand().equals("회원가입")) {
-			vo = new ClickerUserVO();
-			if(txtID.getText()!=null && new String(passwordField.getPassword())!=null && new String(passwordField_1.getPassword())!=null) {
-				
-				if(new String(passwordField.getPassword()).equals(new String(passwordField_1.getPassword()))) {	//비밀번호-비밀번호 확인의 정보 비교
-					//초기 설정 값
-					vo.setId(txtID.getText());	
-					vo.setPwd(new String(passwordField.getPassword()));
-					vo.setDurability(pick.getDura());
-					vo.setEnhance(pick.getLevel());
-					vo.setGold(pick.getMoney());
-					vo.setPickLevel(pick.getPickLevel());
-					vo.setScore(pick.getScore());
-					vo.setPickName(pick.getPickName());
-					vo.setDamage(pick.getDmg());
-					vo.setMul(pick.getMul());
-					vo.setProgress(0);		//엔딩확인 초기값
-					
-//					txtID.setText("");
-					passwordField.setText("");
-					passwordField_1.setText("");						
-					
-					int result=dao.insertUser(vo);
-					if(result>0) {						
-						JOptionPane.showMessageDialog(this, "가입 완료");
-						pick.setUserId(txtID.getText());
-						MiddlePage mp = new MiddlePage();
-//						mp.playInfo();
-					}else {
-						JOptionPane.showMessageDialog(this, "아이디 중복");
-					}				
-				}else if(!(new String(passwordField.getPassword()).equals(new String(passwordField_1.getPassword())))) {
-					JOptionPane.showMessageDialog(this, "비밀번호가 일치하지 않습니다.");
-				}				
-			}else if(txtID.getText()==null || new String(passwordField.getPassword())==null || new String(passwordField_1.getPassword())==null) {
-				JOptionPane.showMessageDialog(this, "정보를 모두 기입해 주세요");
-			}
-		}
+		// TODO Auto-generated method stub
+		
 	}
-}
+	}
+	
+
+//	@Override
+//	public void actionPerformed(ActionEvent e) {
+//		if(e.getActionCommand().equals("회원가입")) {
+//			vo = new ClickerUserVO();
+//			if(txtID.getText()!=null && new String(passwordField.getPassword())!=null && new String(passwordField1.getPassword())!=null) {
+//				
+//				if(new String(passwordField.getPassword()).equals(new String(passwordField1.getPassword()))) {	//비밀번호-비밀번호 확인의 정보 비교
+//					//초기 설정 값
+//					vo.setId(txtID.getText());	
+//					vo.setPwd(new String(passwordField.getPassword()));
+//					vo.setDurability(pick.getDura());
+//					vo.setEnhance(pick.getLevel());
+//					vo.setGold(pick.getMoney());
+//					vo.setPickLevel(pick.getPickLevel());
+//					vo.setScore(pick.getScore());
+//					vo.setPickName(pick.getPickName());
+//					vo.setDamage(pick.getDmg());
+//					vo.setMul(pick.getMul());
+//					vo.setProgress(0);		//엔딩확인 초기값
+//					
+////					txtID.setText("");
+//					passwordField.setText("");
+//					passwordField1.setText("");						
+//					
+//					int result=dao.insertUser(vo);
+//					if(result>0) {						
+//						JOptionPane.showMessageDialog(this, "가입 완료");
+//						pick.setUserId(txtID.getText());
+//						MiddlePage mp = new MiddlePage();
+////						mp.playInfo();
+//					}else {
+//						JOptionPane.showMessageDialog(this, "아이디 중복");
+//					}				
+//				}else if(!(new String(passwordField.getPassword()).equals(new String(passwordField1.getPassword())))) {
+//					JOptionPane.showMessageDialog(this, "비밀번호가 일치하지 않습니다.");
+//				}				
+//			}else if(txtID.getText()==null || new String(passwordField.getPassword())==null || new String(passwordField1.getPassword())==null) {
+//				JOptionPane.showMessageDialog(this, "정보를 모두 기입해 주세요");
+//			}
+//		}
+//	}
+//}
