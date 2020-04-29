cac4d6493cee5cb47cdaffbda5e7322daf819c52
yoonjunghwan <glglgl47@naver.com>
2020-04-29 11:41:11 +0900

    04291141

--- src/ore/Ore.java
+++ src/ore/Ore.java
@@ -13,16 +13,16 @@ public class Ore {
 	public int hit(int atk) {		
 		if(hp > 0 && hp > atk) {
 			hp = hp - atk;
-			System.out.println(name+ " : " + hp);
+//			System.out.println(name+ " : " + hp);
 			return 0;
 		} else {
 			if(hp > 0 && hp <= atk) {
 				hp = 0;
-				System.out.println(name + " : " + hp);
-				System.out.println(name + "채광 완료");
+//				System.out.println(name + " : " + hp);
+//				System.out.println(name + "채광 완료");
 				return reward;
 			} else {
-				System.out.println("더 이상 채광할 수 없습니다.");
+//				System.out.println("더 이상 채광할 수 없습니다.");
 				return 0;
 			}
 		}
